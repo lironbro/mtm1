354ddd58082a31ee6209568b4b68b3dd2f22305d
lironbro <liron.bronfman@gmail.com>
2016-03-30 00:17:43 +0300

    Added files via upload

--- apartment.c
+++ apartment.c
@@ -24,36 +24,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include "apartment.h"
 
-typedef enum {EMPTY, WALL} SquareType;
-typedef enum {APARTMENT_NULL_ARG, APARTMENT_OUT_OF_MEM, APARTMENT_SUCCESS,
-				APARTMENT_OUT_OF_BOUNDS, APARTMENT_NO_ROOM} ApartmenmtResult;
-typedef struct apartment_t Apartment;
 
-struct apartment_t{
-	SquareType** squares;
-	int width;
-	int length;
-	int price;
-};
-
-Apartment apartmentCreate(SquareType** squares, int length, int width, int price);
-
-ApartmenmtResult apartmentIsSameRoom(Apartment apartment, int row1, int col1,
-		 int row2, int col2, bool* outResult);
-
-int apartmentTotalArea(Apartment apartment);
-
-ApartmenmtResult apartmentRoomArea(Apartment apartment, int row, int col,
-			int* outArea);
-
-bool apartmentIsIdentical(Apartment apartment1, Apartment apartment2);
-
-int apartmentGetPrice(Apartment apartment);
-
-Apartment apartmentCopy(Apartment apartment);
-
-void apartmentDestroy(Apartment apartment);
+void checkPath(Apartment apartment, int currentRow,
+		int currentCol, int destinationRow, int destinationCol, bool* outResult);
 
 
 
@@ -62,79 +37,85 @@ Apartment apartmentCreate(SquareType** squares, int length, int width, int price
 {
 	if(*squares == NULL || length<= 0 || width<= 0 || price< 0)
 		return NULL;
-	Apartment apt;
-	apt.length = length;
-	apt.width = width;
-	apt.price = price;
-	apt.squares = squares;
-	return apt;
+	Apartment apartment;
+	apartment->length = length;
+	apartment->width = width;
+	apartment->price = price;
+	apartment->squares = malloc(length*sizeof(SquareType));  // איך אני עושה מערך של סקוואר טייפ?
+	for(int i=0; i<length; i++) 	// בהשראת איתמר
+		apartment->squares[i] = malloc(width*sizeof(SquareType));	// לא יודע אם זה יעבוד
+	return apartment;
 }
 
-ApartmenmtResult apartmentIsSameRoom(Apartment apartment, int row1, int col1,
+ApartmentResult apartmentIsSameRoom(Apartment apartment, int row1, int col1,
 		 int row2, int col2, bool* outResult)
 {
-	if(row1>=apartment.width || col1>=apartment.length
-			|| row2>=apartment.width || col2>=apartment.length)
+	if(row1>=apartment->width || col1>=apartment->length
+			|| row2>=apartment->width || col2>=apartment->length)
 		return APARTMENT_OUT_OF_BOUNDS;
-	if(apartment.squares[row1][col1] == WALL || apartment.squares[row2][col2] == WALL)
+	if(apartment->squares[row1][col1] == WALL || apartment->squares[row2][col2] == WALL)
 		return APARTMENT_NO_ROOM;
 	checkPath(apartment, row1, col1, row2, col2, outResult);
 	return APARTMENT_SUCCESS;
 }
 
-void checkPath(Apartment apartment, int currentRow, int currentCol, // צריך לבדוק, נראה לי שיעבוד
-		int destinationRow, int destinationCol, bool* outResult)
+void checkPath(Apartment apartment, int currentRow, int currentCol, int destinationRow, int destinationCol, bool* outResult)
 {
 	if(currentRow == destinationRow && currentCol == destinationCol)
 	{
 		*outResult = true;
 		return;
 	}
-	apartment.squares[currentRow][currentCol] = WALL;
+	apartment->squares[currentRow][currentCol] = WALL;
 
-	if(currentRow+1 < apartment.length &&
-			apartment.squares[currentRow+1][currentCol] == EMPTY)
+	if(currentRow+1 < apartment->length &&
+			apartment->squares[currentRow+1][currentCol] == EMPTY)
 		checkPath(apartment, currentRow+1, currentCol, destinationRow, destinationCol, outResult);
 
-	if(currentRow-1 >= 0 && apartment.squares[currentRow-1][currentCol] == EMPTY)
+	if(currentRow-1 >= 0 && apartment->squares[currentRow-1][currentCol] == EMPTY)
 		checkPath(apartment, currentRow-1, currentCol, destinationRow, destinationCol, outResult);
 
-	if(currentCol+1 < apartment.width &&
-			apartment.squares[currentRow][currentCol+1] == EMPTY)
+	if(currentCol+1 < apartment->width &&
+			apartment->squares[currentRow][currentCol+1] == EMPTY)
 		checkPath(apartment, currentRow+1, currentCol, destinationRow, destinationCol, outResult);
 
-	if(currentRow-1 >= 0 && apartment.squares[currentRow][currentCol-1] == EMPTY)
+	if(currentRow-1 >= 0 && apartment->squares[currentRow][currentCol-1] == EMPTY)
 		checkPath(apartment, currentRow-1, currentCol, destinationRow, destinationCol, outResult);
 
-	apartment.squares[currentRow][currentCol] = EMPTY;
+	apartment->squares[currentRow][currentCol] = EMPTY;
 }
 
 int apartmentTotalArea(Apartment apartment)
 {
 	int empties = 0;
-	for(int i=0; i<apartment.length; i++)
-		for(int j=0; j<apartment.width; j++)
-			if(apartment.squares[i][j] == EMPTY)
+	for(int i=0; i<apartment->length; i++)
+		for(int j=0; j<apartment->width; j++)
+			if(apartment->squares[i][j] == EMPTY)
 				empties++;
 	return empties;
 }
 
-ApartmenmtResult apartmentRoomArea(Apartment apartment, int row, int col,
+
+/*
+ApartmentResult apartmentRoomArea(Apartment apartment, int row, int col,
 			int* outArea)
 {	// אפשר להתחיל בקורדינטה ולהתפשט על כל השטח של החדר
 	int squares = 0;
-	for(int i=0; i<apartment.length; i++)
+	for(int i=0; i<apartment->length; i++)
 	{
-		for(int j=0; j<apartment.width; j++)
+		for(int j=0; j<apartment->width; j++)
 		{
 			if(i!=row || j!=col)
 			{
 				bool sameRoom = false;
-				apartmentIsSameRoom(apartment, row, col, i, j)
+				apartmentIsSameRoom(apartment, row, col, i, j);
 			}
 		}
 	}
+	return APARTMENT_SUCCESS;
 }
+*/
+
 
 bool apartmentIsIdentical(Apartment apartment1, Apartment apartment2)
 {
@@ -142,19 +123,19 @@ bool apartmentIsIdentical(Apartment apartment1, Apartment apartment2)
 		return true;
 	if(apartment1 == NULL || apartment2 == NULL)
 		return false;
-	if(apartment1.price != apartment2.price || apartment1.length != apartment2.length ||
-			apartment1.width!= apartment2.width)
+	if(apartment1->price != apartment2->price || apartment1->length != apartment2->length ||
+			apartment1->width!= apartment2->width)
 		return false;
-	for(int i=0; i<apartment1.length; i++)
-		for(int j=0; j<apartment1.width; j++)
-			if(apartment1.squares[i][j] != apartment2.squares[i][j])
+	for(int i=0; i<apartment1->length; i++)
+		for(int j=0; j<apartment1->width; j++)
+			if(apartment1->squares[i][j] != apartment2->squares[i][j])
 				return false;
 	return true;
 }
 
 int apartmentGetPrice(Apartment apartment)
 {
-	return apartment.price;
+	return apartment->price;
 }
 
 
@@ -163,13 +144,16 @@ Apartment apartmentCopy(Apartment apartment)
 	if(apartment == NULL)
 		return NULL;
 	Apartment copy = malloc(sizeof(Apartment));
-	copy.length = apartment.length;
-	copy.width = apartment.width;
-	copy.price = apartment.price;
-	copy.squares = malloc(copy.length*copy.width*sizeof(SquareType)); // רצוי לבדוק אם זה נכון
-	for(int i=0; i<copy.length; i++)
-		for(int j=0; j<copy.width; j++)
-			copy.squares[i][j] = apartment.squares[i][j];
+	copy->length = apartment->length;
+	copy->width = apartment->width;
+	copy->price = apartment->price;
+	copy->squares = malloc(copy->length*sizeof(SquareType)); // רצוי לבדוק אם זה נכון
+	for(int i=0; i<copy->length; i++)
+	{
+		copy->squares[i] = malloc(copy->width*sizeof(SquareType));
+		for(int j=0; j<copy->width; j++)
+			copy->squares[i][j] = apartment->squares[i][j];
+	}
 	return copy;
 }
 
@@ -177,10 +161,10 @@ void apartmentDestroy(Apartment apartment)
 {
 	if(apartment == NULL)
 		return;
-	free(apartment.length);
-	free(apartment.width);
-	free(apartment.price);
-	free(apartment.squares);
+	free(apartment->length);
+	free(apartment->width);
+	free(apartment->price);
+	free(apartment->squares);
 }
 
 
--- apartment_service.c
+++ apartment_service.c
@@ -8,33 +8,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <apartment.c>
+#include "apartment.h"
+#include "apartment_service.h"
 
 
+void quickSort( int a[], int l, int r);
 
-typedef struct apartment_service_t* ApartmentService;
-typedef enum {APARTMENT_SERVICE_NULL_ARG, APARTMENT_SERVICE_OUT_OF_MEM,
-		APARTMENT_SERVICE_SUCCESS, APARTMENT_SERVICE_OUT_OF_BOUNDS,
-		APARTMENT_SERVICE_FULL, APARTMENT_SERVICE_ALREADY_EXISTS,
-		APARTMENT_SERVICE_EMPTY, } ApartmentServiceResult;
-
-struct apartment_service_t
-{
-			int numOfApartments;
-			int maxNumOfApartments;
-			Apartment apartments[];
-			int ids[];
-};
-
-ApartmentService serviceCreate(int maxNumOfApartments);
-
-ApartmentServiceResult serviceAddApartment(ApartmentService service,
-			Apartment apartment, int id);
-
-int serviceNumberOfApatments(ApartmentService service);
-
-ApartmentServiceResult servicePriceMedian(ApartmentService service,
-		int* outResult);
+int partition( int a[], int l, int r);
 
 
 
@@ -45,8 +25,8 @@ ApartmentService serviceCreate(int maxNumOfApartments)
 	ApartmentService service;  // מה הוא רוצה פההההההה
 	service->maxNumOfApartments = maxNumOfApartments;
 	service->numOfApartments = 0;
-	service->apartments[maxNumOfApartments];
-	service->ids[maxNumOfApartments];
+	service->apartments = malloc(maxNumOfApartments *sizeof(Apartment));
+	service->ids = malloc(maxNumOfApartments *sizeof(int));
 	return service;
 }
 
@@ -81,7 +61,7 @@ ApartmentServiceResult servicePriceMedian(ApartmentService service,
 	int length = service->numOfApartments;
 	int prices[length];
 	for(int i=0; i<length; i++)
-		prices[i] = service->apartments[i].price;
+		prices[i] = service->apartments[i]->price;
 	quickSort(prices, 0, length-1);
 	if(length%2 == 0)
 		*outResult = (prices[length/2]+prices[length/2+1])/2;
